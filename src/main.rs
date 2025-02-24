e5752c86b9a4a91a114ad065eb40ef2138b3999c
jakkie <jakubkielbasiewicz@gmail.com>
2025-02-24 01:24:00 +0100

    init klines logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+
+use anyhow::Result;
+use chrono::NaiveDate;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Starting Jan 1st, adjust year if needed.
+    let start_date = NaiveDate::from_ymd(2025, 1, 1);
+    let end_date = chrono::Utc::today().naive_utc();
+    let symbols = vec!["BTC", "SOL"];
+    
+    for symbol in symbols {
+        let mut all_klines = Vec::new();
+        let mut current_date = start_date;
+        // Loop through each day.
+        while current_date <= end_date {
+            println!("Fetching {} klines for {}", symbol, current_date);
+            match fetch_klines_for_date(symbol, current_date).await {
+                Ok(data) => {
+                    println!("Got {} klines for {}", data.len(), current_date);
+                    all_klines.extend(data);
+                },
+                Err(e) => {
+                    println!("Error fetching {} on {}: {}", symbol, current_date, e);
+                },
+            }
+            current_date = current_date.succ();
+        }
+        store_klines(symbol, &all_klines)?;
+    }
+    Ok(())
 }
